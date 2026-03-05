5b24f8403f83d059fb585f9bb6b48817d60d83ac
jimmy <lettevoueto.jimmy@gmail.com>
2026-03-05 16:15:27 +0100

    feat(card): implémentation de la logique de comparaison

--- src/card.py
+++ src/card.py
@@ -1,7 +1,19 @@
 class Card:
+    RANK_VALUES = {
+        '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9,
+        '10': 10, 'J': 11, 'Q': 12, 'K': 13, 'A': 14
+    }
+
     def __init__(self, rank, suit):
         self.rank = rank
         self.suit = suit
+        self.value = self.RANK_VALUES[rank]
 
-    def __str__(self):
-        return f"{self.rank}{self.suit}"
+    def __gt__(self, other):
+        return self.value > other.value
+
+    def __eq__(self, other):
+        return self.value == other.value
+
+    def __lt__(self, other):
+        return self.value < other.value
