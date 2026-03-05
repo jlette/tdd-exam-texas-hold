10ed6e7f9e3c64e0637a6dc0c032b5e3915060ea
jimmy <lettevoueto.jimmy@gmail.com>
2026-03-05 16:14:41 +0100

    test(card): comparaisons des cartes

--- tests/test_card.py
+++ tests/test_card.py
@@ -1,7 +1,22 @@
 from src.card import Card
 
-def test_card_attributes():
+def test_card_init():
     card = Card('A', 'H')
     assert card.rank == 'A'
     assert card.suit == 'H'
     assert card.value == 14
+    
+def test_should_consider_king_stronger_than_queen():
+    stronger_card = Card("K", "H")
+    weaker_card = Card("Q", "S")
+    assert stronger_card > weaker_card
+
+def test_should_consider_cards_equal_rank_as_equivalent_strength():
+    card_hearts = Card("10", "H")
+    card_spades = Card("10", "S")
+    assert card_hearts == card_spades
+
+def test_should_identify_ace_as_highest_rank_by_default():
+    ace = Card("A", "D")
+    king = Card("K", "C")
+    assert ace > king
